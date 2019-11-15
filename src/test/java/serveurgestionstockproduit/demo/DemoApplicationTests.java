483b609eae8de420005508cefa01247b933ff249
DjafriMouradGit <djafrimourad.jobs@gmail.com>
2019-11-15 15:48:49 +0100

    first commit

---
+++ src/test/java/serveurgestionstockproduit/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package serveurgestionstockproduit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
